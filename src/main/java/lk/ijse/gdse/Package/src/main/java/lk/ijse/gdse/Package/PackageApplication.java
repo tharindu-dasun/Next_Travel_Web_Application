0f852f4b7f2849d54bcdd63986b9885430b61565
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-20 23:32:30 +0530

    Package_Service Package Added

---
+++ src/main/java/lk/ijse/gdse/Package/src/main/java/lk/ijse/gdse/Package/PackageApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.Package.src.main.java.lk.ijse.gdse.Package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackageApplication.class, args);
+	}
+
+}
