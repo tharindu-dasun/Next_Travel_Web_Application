763b28c91900f828a9f32203a0d5ec97d1229e6e
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-20 23:29:17 +0530

    Vehicale_Service Package Added

---
+++ src/main/java/lk/ijse/gdse/Vehicale/src/main/java/lk/ijse/gdse/Vehicale/VehicaleApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.Vehicale.src.main.java.lk.ijse.gdse.Vehicale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicaleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/lk/ijse/gdse/Vehicale/src/test/java/lk/ijse/gdse/Vehicale/VehicaleApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.Vehicale.src.test.java.lk.ijse.gdse.Vehicale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
