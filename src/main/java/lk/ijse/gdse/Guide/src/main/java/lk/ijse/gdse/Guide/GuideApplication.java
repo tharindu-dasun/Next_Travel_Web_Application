5850aa98b08f470d7aac668b99e0233ed35c8a6d
tharindu-dasun <tharindu.dasunccp@gmail.com>
2023-11-20 23:20:45 +0530

    Guide_Service Package Added

---
+++ src/main/java/lk/ijse/gdse/Guide/src/main/java/lk/ijse/gdse/Guide/GuideApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse.Guide.src.main.java.lk.ijse.gdse.Guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuideApplication.class, args);
+	}
+
+}
